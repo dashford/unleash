3b0d895f81ca9b4789258ed10c2ddd56b01d8e5f
David Ashford <ashford.david@gmail.com>
2019-03-31 20:45:38 +0100

    Stash

--- src/Client.php
+++ src/Client.php
@@ -4,13 +4,43 @@ declare(strict_types=1);
 
 namespace Dashford\Unleash;
 
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Psr7\Request;
+
 class Client
 {
+    /** @var Config */
+    private $config;
+
+    /** @var HttpClient */
+    private $httpClient;
+
+    public function __construct(Config $config, HttpClient $httpClient)
+    {
+        $this->config = $config;
+        $this->httpClient = $httpClient;
+    }
+
     public function isEnabled(string $feature, $context = null, bool $default = false): bool
     {
         if ($feature === 'does-not-exist') {
             return $default;
         }
         return true;
+    }
+
+    private function registerClient()
+    {
+        $headers = [];
+        $body = [
+            'appName' => $this->config->getAppName(),
+            'instanceId' => $this->config->getInstanceId(),
+            'sdkVersion' => $this->config->getSDKVersion(),
+            'strategies' => [],
+            'started' => $this->config->getStartTime(),
+            'interval' => $this->config->getMetricsInterval()
+        ];
+        $request = new Request('POST', 'http://localhost:9090', $headers, $body);
+        $promise = $this->httpClient->sendAsync($request);
     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -4,20 +4,39 @@ declare(strict_types=1);
 
 namespace Dashford\Unleash\Tests;
 
+use Dashford\Unleash\Config;
 use PHPUnit\Framework\TestCase;
 use Dashford\Unleash\Client;
 
 class ClientTest extends TestCase
 {
+    /** @var Config */
+    private $mockConfig;
+
+    /** @var \GuzzleHttp\Client */
+    private $mockHttpClient;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->mockConfig = $this->createMock(Config::class);
+        $this->mockHttpClient = $this->createMock(\GuzzleHttp\Client::class);
+    }
+
+    public function testRegistrationApiCallIsMade()
+    {
+
+    }
+
     public function testFeatureIsEnabled()
     {
-        $client = new Client();
+        $client = new Client($this->mockConfig, $this->mockHttpClient);
         $this->assertTrue($client->isEnabled('test-feature'));
     }
 
     public function testDefaultValueIsReturned()
     {
-        $client = new Client();
+        $client = new Client($this->mockConfig, $this->mockHttpClient);
         $this->assertFalse($client->isEnabled('does-not-exist', null, false));
         $this->assertTrue($client->isEnabled('does-not-exist', null, true));
     }
